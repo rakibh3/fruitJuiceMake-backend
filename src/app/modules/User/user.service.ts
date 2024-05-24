c6aa79ee462d2fca20c1d6470aae1fcbccaf111c
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-05-24 22:55:30 +0600

    Create JWT token when user will login

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,15 +1,41 @@
+import config from '../../config'
+import { generateToken } from '../../helper/generateToken'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 
 const userLoginIntoBD = async (payLoad: TUser) => {
-  const result = await User.findOne({ email: payLoad.email })
-  if (result) {
-    return result
-  } else {
-    const newUser = new User(payLoad)
-    await newUser.save()
-    return newUser
+  let user = await User.findOne({ email: payLoad.email })
+
+  // If user not exist then create a new user
+  if (!user) {
+    user = new User(payLoad)
+    await user.save()
   }
+
+  // Payload for jwt
+  const jwtPayload = {
+    id: user._id,
+    email: user.email,
+    displayName: user.displayName,
+  }
+
+  // Generate JWT Token
+  const accessToken = generateToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  )
+
+  const userData = {
+    _id: user._id,
+    displayName: user.displayName,
+    email: user.email,
+    photoURL: user.photoURL,
+    coin: user.coin,
+    accessToken,
+  }
+
+  return userData
 }
 
 export const UserService = {
