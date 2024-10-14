0d02c983c1f207dfa5583183601fbe6afc565be7
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-10-14 19:23:24 +0600

    config: add vercel.json for project configuration

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(
   cors({
     origin: [
-      'https://recipe-rakib03.web.app',
+      'https://juicers-cv.vercel.app',
       'http://localhost:5173',
       'http://localhost:4173',
     ],
